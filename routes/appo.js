64e5a7d33ffc670fcf23f5a24521eefb333f6feb
WangQiang-DREAM <826011374@qq.com>
2018-05-11 10:00:02 +0800

    wq

--- routes/appo.js
+++ routes/appo.js
@@ -7,7 +7,7 @@ router.get('/queryAppoInfo', appoController.queryAppoInfo);
 // 修改预约状态
 router.get('/changeAppoStatus',appoController.changeAppoStatus);
 
-//添加预约
+//添加预约接口
 router.get('/addAppo',appoController.addAppo)
 
 module.exports = router;
